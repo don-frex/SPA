4379d7d4b87325d98792e401d455f1bd8e2b8451
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-12-09 19:20:21 +0100

    add games history

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
 
 // css display flex
 // css display grid
